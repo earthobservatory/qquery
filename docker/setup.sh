6af94c8c0c905f2612fbfda9b0b91abfb07d6c32
chinshitong01 <stchin@ntu.edu.sg>
2018-09-04 17:41:51 +0800

    BUGS

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Pulls in ARIA repos outside qquery
-scihub_url="https://github.com/hysds/scihub.git"
-asf_url="https://github.com/hysds/asf-query.git"
-unavco_url="https://github.com/hysds/unavco-query.git"
-apihub_url="https://github.com/hysds/apihub.git"
+scihub_url="https://github.com/earthobservatory/scihub.git"
+asf_url="https://github.com/earthobservatory/asf-query.git"
+unavco_url="https://github.com/earthobservatory/unavco-query.git"
+apihub_url="https://github.com/earthobservatory/apihub.git"
 # revert back later
 git clone $scihub_url -b "chin_test" "scihub"
 # git clone $scihub_url "scihub"
